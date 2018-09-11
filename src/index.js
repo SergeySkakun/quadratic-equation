7fbedb8cd98842dae97e59e05ea33a6c048e6ac8
SergeySkakun <thexfiles116@gmail.com>
2018-09-11 15:48:06 +0300

    quadratic-equation/feature: implement fucntion solveEquation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let array = equation.split(" ");
+
+  let a = +array[0];
+  let b = +(array[3] + array[4]);
+  let c = +(array[7]+ array[8]);
+  let x1, x2;
+
+  let D = b * b - 4 * a * c;
+
+  x1 = Math.round((-b + Math.sqrt(D)) / (2 * a));
+  x2 = Math.round((-b - Math.sqrt(D)) / (2 * a));
+
+  return x1 < x2 ? [x1, x2] : [x2, x1];
 }
